c6a3e77fe8f679c5496a84bf0f44b849c32be97b
Vivek Gidmare <vivekgidmare@gmail.com>
2016-06-03 14:12:01 +0530

    Repository for implementation of search and sort algorithms for fun.

---
+++ app/src/main/java/com/algorithms/MainActivity.java
@@ -0,0 +1,82 @@
+package com.algorithms;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = MainActivity.class.getSimpleName();
+    private int[] array = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    private int[] selectionSortArray = {61, 67, 71, 73, 2, 3, 53, 59, 79, 83, 89, 97, 5, 7, 11, 13, 37, 41, 43, 47, 17, 19, 23, 29, 31};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Size :" + array.length);
+
+        //Binary Search Testing
+        int result = binarySearch(67);
+        Log.i(TAG, "Search Result ::" + result);
+        int result2 = binarySearch(62);
+        Log.i(TAG, "Search result2 ::" + result2);
+
+        //Selection sort testing
+        Log.i(TAG, "Before selection sort\n:" + Arrays.toString(selectionSortArray));
+        selectionSort(selectionSortArray);
+        Log.i(TAG, "After selection sort\n:" + Arrays.toString(selectionSortArray));
+
+    }
+
+    private int binarySearch(int input) {
+        int min = 0;
+        int max = array.length - 1;
+        int guess;
+        while (max >= min) {
+            guess = (int) Math.floor((min + max) / 2);
+            if (array[guess] == input)
+                return guess;
+            if (array[guess] < input) {
+                min = guess + 1;
+            } else {
+                max = guess - 1;
+            }
+            Log.i(TAG, "*" + guess);
+        }
+        return -1;
+    }
+
+    //-------------Selection Sort---------------
+
+    private void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    private int indexOfMinimum(int[] array, int startIndex) {
+        int minIndex = startIndex;
+        int minValue = array[startIndex];
+        for (int i = minIndex + 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minIndex = i;
+                minValue = array[i];
+            }
+        }
+        return minIndex;
+    }
+
+    private void selectionSort(int[] array) {
+        int minIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            minIndex = indexOfMinimum(array, i);
+            swap(array, minIndex, i);
+        }
+
+    }
+    //-------------Selection Sort---------------
+}
